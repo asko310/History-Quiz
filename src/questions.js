e2751ddf4f7b954d39a5856c1de12af226976292
asko310 <129404218+asko310@users.noreply.github.com>
2024-09-15 17:19:57 +0300

    a bitta js....

---
+++ src/questions.js
@@ -0,0 +1,27 @@
+//array care contine numarul, intrebarea, optiunile si raspunsurile
+let questions = [
+    {
+    numb:1,
+    question: "Cati ani are Dumitru?",
+    answer: "13",
+    options: [
+        "12",
+        "11",
+        "14",
+        "13"
+    ]
+
+    },
+    {
+        numb:2,
+        question: "Care e raspunsul corect?",
+        answer: "55",
+        options: [
+            "22",
+            "44",
+            "33",
+            "66"
+        ]
+    
+        },
+]
---
+++ src/script.js
@@ -0,0 +1,3 @@
+//selectam toate elementele necesare
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box")
